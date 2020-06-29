87b0b56b54bc97b84e5f6e9ebf56c4a6e1952719
tunde <icepaigez@gmail.com>
2020-06-29 23:32:59 +0100

    fixed a port typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const compression = require("compression");
 
 const server = express();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.use(compression());
 server.use(express.static(path.join(__dirname, 'build')));
